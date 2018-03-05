4a175e0d5d68f80e6619900df79a1d80c9c8c2f0
ckummrow <chkummro@microsoft.com>
2018-03-04 21:12:12 -0600

    Initial commit

---
+++ Tornado Firmware/Tornado/Debug/nozzle.cpp.d
@@ -0,0 +1,33 @@
+C:\Users\Robert\AppData\Local\Temp\VMBuilds\Tornado\mega_atmega2560\Debug\nozzle.cpp.o: \
+ C:\Users\Robert\AppData\Local\Temp\VMBuilds\Tornado\mega_atmega2560\Debug\nozzle.cpp \
+ C:\Users\Robert\AppData\Local\Temp\VMBuilds\Tornado\mega_atmega2560\Debug\nozzle.h \
+ C:\Users\Robert\AppData\Local\Temp\VMBuilds\Tornado\mega_atmega2560\Debug\Marlin.h \
+ C:\Users\Robert\AppData\Local\Temp\VMBuilds\Tornado\mega_atmega2560\Debug\MarlinConfig.h \
+ C:\Users\Robert\AppData\Local\Temp\VMBuilds\Tornado\mega_atmega2560\Debug\fastio.h \
+ C:\Users\Robert\AppData\Local\Temp\VMBuilds\Tornado\mega_atmega2560\Debug\macros.h \
+ C:\Users\Robert\AppData\Local\Temp\VMBuilds\Tornado\mega_atmega2560\Debug\fastio_1280.h \
+ C:\Users\Robert\AppData\Local\Temp\VMBuilds\Tornado\mega_atmega2560\Debug\boards.h \
+ C:\Users\Robert\AppData\Local\Temp\VMBuilds\Tornado\mega_atmega2560\Debug\Version.h \
+ C:\Users\Robert\AppData\Local\Temp\VMBuilds\Tornado\mega_atmega2560\Debug\Configuration.h \
+ C:\Users\Robert\AppData\Local\Temp\VMBuilds\Tornado\mega_atmega2560\Debug\Conditionals_LCD.h \
+ C:\Users\Robert\AppData\Local\Temp\VMBuilds\Tornado\mega_atmega2560\Debug\Configuration_adv.h \
+ C:\Users\Robert\AppData\Local\Temp\VMBuilds\Tornado\mega_atmega2560\Debug\pins.h \
+ C:\Users\Robert\AppData\Local\Temp\VMBuilds\Tornado\mega_atmega2560\Debug\pins_MKS_BASE.h \
+ C:\Users\Robert\AppData\Local\Temp\VMBuilds\Tornado\mega_atmega2560\Debug\pins_RAMPS.h \
+ C:\Users\Robert\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\cores\arduino/Arduino.h \
+ C:\Users\Robert\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\cores\arduino/binary.h \
+ C:\Users\Robert\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\cores\arduino/WCharacter.h \
+ C:\Users\Robert\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\cores\arduino/WString.h \
+ C:\Users\Robert\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\cores\arduino/HardwareSerial.h \
+ C:\Users\Robert\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\cores\arduino/USBAPI.h \
+ C:\Users\Robert\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\cores\arduino/Arduino.h \
+ C:\Users\Robert\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\variants\mega/pins_arduino.h \
+ C:\Users\Robert\AppData\Local\Temp\VMBuilds\Tornado\mega_atmega2560\Debug\Conditionals_post.h \
+ C:\Users\Robert\AppData\Local\Temp\VMBuilds\Tornado\mega_atmega2560\Debug\SanityCheck.h \
+ C:\Users\Robert\AppData\Local\Temp\VMBuilds\Tornado\mega_atmega2560\Debug\enum.h \
+ C:\Users\Robert\AppData\Local\Temp\VMBuilds\Tornado\mega_atmega2560\Debug\types.h \
+ C:\Users\Robert\AppData\Local\Temp\VMBuilds\Tornado\mega_atmega2560\Debug\utility.h \
+ C:\Users\Robert\AppData\Local\Temp\VMBuilds\Tornado\mega_atmega2560\Debug\serial.h \
+ C:\Users\Robert\AppData\Local\Temp\VMBuilds\Tornado\mega_atmega2560\Debug\MarlinSerial.h \
+ C:\Users\Robert\AppData\Local\Temp\VMBuilds\Tornado\mega_atmega2560\Debug\stopwatch.h \
+ C:\Users\Robert\AppData\Local\Temp\VMBuilds\Tornado\mega_atmega2560\Debug\point_t.h
